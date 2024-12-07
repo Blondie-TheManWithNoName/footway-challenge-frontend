29bd933191b8ce4a8c88fd93877615b436019254
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-12-07 13:00:14 +0100

    update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        picked: "#6D11A7",
+        // picked2: "#EEDAFB",
+        picked2: "#f0f0f0",
+        picked3: "#e0e0e0",
+        "violet-100": "#531A74",
+        "violet-400": "#321046",
+        "violet-700": "#300F43",
+        "violet-800": "#240B32",
+        default: "#FFFFFF",
+        disabled: "#75aaff",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
