81c46de41ef4d146e261d755321f663cdc1d3d1b
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-12-08 00:38:26 +0100

    basuc routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import "./App.css";
 
 import { MappingsProvider } from "./contexts/MappingContext";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import Mapping from "./Mapping";
+import Home from "./components/ui/Home";
 
 function App() {
   return (
     <MappingsProvider>
       <main className="w-screen h-screen">
-        <Mapping />
-
+        <Router>
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/mapping">Mapping</Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mapping" element={<Mapping />} />
+            {/* <Route path="*" element={<NotFound />} /> */}
+          </Routes>
+        </Router>
         {/* <div className="md:hidden flex flex-col items-center w-full">
           <Tabs defaultValue="account" className="max-w-xl w-full">
             <TabsList className="w-full">
