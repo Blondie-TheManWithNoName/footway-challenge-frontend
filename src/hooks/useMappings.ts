585c0b31f98bc3688df8bd2ddd0f2f23d17a945e
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-12-11 00:50:10 +0100

    Update useMappings.ts

--- src/hooks/useMappings.ts
+++ src/hooks/useMappings.ts
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { toast } from "sonner";
+import { set } from "zod";
 
 const baseURL = "http://localhost:8080";
 
@@ -8,43 +10,29 @@ export const useMappings = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<unknown | null>(null);
 
-  const getMappings = async (sku: string) => {
+  const getMappings = async ({
+    sku,
+    orderId,
+  }: {
+    sku?: string;
+    orderId: number;
+  }) => {
     try {
       const response = await fetch(
-        `${baseURL}/mappings?${"digitalSku=" + sku}`
+        `${baseURL}/mappings?orderId=${orderId}&${
+          sku ? `digitalSku=${sku}` : ""
+        }`
       );
       const data = await response.json();
       setMappings(data);
+      setIsLoading(false);
     } catch (error) {
       setError(error);
+      setIsLoading(false);
     }
   };
 
-  const getMapping = async (sku: string) => {
-    // try {
-    //   const response = await fetch(`${baseURL}/physical-products/${sku}`);
-    //   const data = await response.json();
-    //   setPhysicalProduct(data);
-    // } catch (error) {
-    //   setError(error);
-    // }
-  };
-
-  const editMapping = async (id: string, data: Object) => {
-    // try {
-    //   const response = await fetch(`${baseURL}/physical-products/${id}`, {
-    //     method: "PUT",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(data),
-    //   });
-    //   const responseData = await response.json();
-    //   setPhysicalProduct(responseData);
-    // } catch (error) {
-    //   setError(error);
-    // }
-  };
+  const getMapping = async (id: number) => {};
 
   const createMapping = async (data: Object) => {
     try {
